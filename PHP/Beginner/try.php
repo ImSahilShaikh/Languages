864c3f70c9521065ea751bb06e3ad2cc9207c080
ImSahilShaikh <43376638+ImSahilShaikh@users.noreply.github.com>
2019-09-06 00:04:06 +0530

    Create try.php

---
+++ PHP/Beginner/try.php
@@ -0,0 +1,15 @@
+<?php
+	//echo "Hello";
+	$s=5;
+	if(empty($_POST['name']))
+	
+	if ( $s==6 )
+	{
+		echo "I'm doing Php";
+	}
+	else
+	{
+		die ("This page will die");
+	}
+	echo "I know this does'nt makes sense";
+?>
