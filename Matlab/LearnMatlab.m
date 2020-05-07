92f97ad85f7cc4c055cf33dfa9efe965dce61251
Sahil Shaikh <43376638+ImSahilShaikh@users.noreply.github.com>
2020-05-07 16:07:40 +0530

    Update LearnMatlab.m

--- Matlab/LearnMatlab.m
+++ Matlab/LearnMatlab.m
@@ -44,4 +44,141 @@ Variables in the current scope:
 
 Total is 5 elements using 40 bytes
 
-% 
+% -----------------------From andrew ng videos--------------------------
+octave:1> 5+6
+ans =  11
+octave:2> 2-3
+ans = -1
+octave:3> 5*8
+ans =  40
+octave:4> 2^6
+ans =  64
+octave:5> 1 == 2
+ans = 0
+octave:6> 1 ~= 2
+ans = 1
+octave:7> 1 && 0
+ans = 0
+octave:8> 1 || 0
+ans = 1
+octave:9> xor(1,0)
+ans = 1
+octave:10> PS1('>> ')
+>> ps1('>>')
+error: 'ps1' undefined near line 1 column 1
+>> PS1('>>')
+>>PS1('>> ')
+>> a = 3
+a =  3
+>> a = 3; %Semicolon supresses the output
+>> % String assignment
+>> b = " hello world";
+>> b
+b =  hello world
+>> a = pi ;
+>> a
+a =  3.1416
+>> %Alternative for display
+>> disp(a);
+ 3.1416
+>> %Printing Strings
+>> disp(sprintf('2 decimals: %0.2f',a))
+2 decimals: 3.14
+>> %Vectors and matrices
+>> A = [1 2; 3 4; 5 6]
+A =
+
+   1   2
+   3   4
+   5   6
+
+>>  % Other way
+>> B = [1 2;
+> 3 4;
+> 5 6]
+B =
+
+   1   2
+   3   4
+   5   6
+
+>> % row vectors
+>> v = [ 1 2 3]
+v =
+
+   1   2   3
+
+>> % column vector
+>> v = [1;2;3]
+v =
+
+   1
+   2
+   3
+
+>>  % Other useful notation
+>> v = 1:0.2:2
+v =
+
+    1.0000    1.2000    1.4000    1.6000    1.8000    2.0000
+
+>> % explanation start from 1: increment by 0.2: end on 2
+>>
+>> v = 1:6
+v =
+
+   1   2   3   4   5   6
+
+>> % row vector from 1 to 6
+>>
+>> %Generating matrices
+>>
+>> ones(2,3)
+ans =
+
+   1   1   1
+   1   1   1
+
+>> %also
+>> c = 2 * ones(2,3)
+c =
+
+   2   2   2
+   2   2   2
+
+>> %same goes with zeros
+>>
+>> w = zeros(1,3)
+w =
+
+   0   0   0
+
+>> % using rand to get numbers from uniform distribution between 0 and 1
+>> rand(3,3)
+ans =
+
+   0.700425   0.537890   0.303504
+   0.926952   0.627159   0.325354
+   0.461717   0.073282   0.617954
+
+>> %every time output will be different
+>>
+>> %plotting with octave
+>>
+>> w = -6 + sqrt(10)*(randn(1,10000));
+>> hist(w)
+>> % we can plot histogram with 50 bars as
+>> hist(w,50)
+>>
+>> % Identity matrix
+>> eye(4)
+ans =
+
+Diagonal Matrix
+
+   1   0   0   0
+   0   1   0   0
+   0   0   1   0
+   0   0   0   1
+
+>> % help command name gives the documentation
