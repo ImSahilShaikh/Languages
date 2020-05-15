8b087e276c6b7103d132e88b31ad9d37286f38c5
Sahil Shaikh <43376638+ImSahilShaikh@users.noreply.github.com>
2020-05-15 20:57:32 +0530

    Add files via upload

---
+++ Kotlin/Why_everything_has_value.kt
@@ -0,0 +1,14 @@
+fun temp()
+{
+    val temperature = 10
+    val isHot = if(temperature>40) true else false
+    print(isHot)
+}
+
+fun main(args: Array<String>)
+{
+    val isUnit = println("This is an expression")
+    println(isUnit)
+    println("-------Calling Function--------")
+    temp()
+}
